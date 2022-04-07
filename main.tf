dd34348db96908caa76cc8683138bb223f54eebf
Roman Prunov <prunovroman@gmail.com>
2022-04-07 22:13:45 +0500

    main

--- main.tf
+++ main.tf
@@ -21,31 +21,5 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket_prefix = "learn-s3-remote-backend-"
-
-  versioning {
-    enabled = true
-  }
-
-  force_destroy = true
-
-  // lifecycle {
-  //   prevent_destroy = true
-  // }
-}
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock-dynamo"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
