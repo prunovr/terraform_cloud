5c83fcf67ecf5396e38ad259d10288b3b2ce07a2
Roman Prunov <prunovroman@gmail.com>
2022-04-07 21:28:11 +0500

    Add S3 bucket

--- main.tf
+++ main.tf
@@ -21,5 +21,31 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket_prefix = "learn-s3-remote-backend-"
+
+  versioning {
+    enabled = true
+  }
+
+  force_destroy = true
+
+  // lifecycle {
+  //   prevent_destroy = true
+  // }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock-dynamo"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
