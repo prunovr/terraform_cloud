c1c818c375bd7aa0a62d1422aad0a7e139775035
Roman Prunov <prunovroman@gmail.com>
2022-04-07 22:40:43 +0500

    sd

--- main.tf
+++ main.tf
@@ -1,21 +1,18 @@
 provider "aws" {  
   region                   = "us-east-1"
 }
+data "aws_ami" "ubuntu" {    
+    most_recent = true
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+   }
 
-# data "aws_ami" "ubuntu" {    
-#     most_recent = true
-#     filter {
-#         name   = "name"
-#         values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
-#    }
-
-#    owners = ["099720109477"]
-# }
-
+   owners = ["099720109477"]
+}
 resource "aws_instance" "web" {
-//  ami           = data.aws_ami.ubuntu.id
-  ami = "ami-04505e74c0741db8d"
- // instance_type = local.instance_type_map.stage
+  ami           = data.aws_ami.ubuntu.id 
+  instance_type = local.instance_type_map.stage
   count = local.instance_count_map.stage
   tags = {
     Name = "Netology ${count.index + 1 }"
